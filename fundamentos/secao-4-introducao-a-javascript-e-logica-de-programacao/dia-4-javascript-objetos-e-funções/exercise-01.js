66e7b0ec3ba0b067a60335d4519b1d8f3b158551
anadolacio <anadolacio@hotmail.com>
2022-09-25 22:51:28 -0300

    resolução da parte 4 do exercicio 1, dia 4.

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funções/exercise-01.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funções/exercise-01.js
@@ -20,3 +20,9 @@ let info = {
   }
 
   // Parte 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+  for (let key in info) {
+    console.log(info[key]);
+  }
+
+  // Parte 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:

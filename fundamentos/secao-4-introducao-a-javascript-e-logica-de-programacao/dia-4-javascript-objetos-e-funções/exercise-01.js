db0005fe912e881b94a11fec789b593eff65e03c
anadolacio <anadolacio@hotmail.com>
2022-09-25 22:45:22 -0300

    resolução da parte 2 do exercicio 1, dia 4.

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funções/exercise-01.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funções/exercise-01.js
@@ -1,9 +1,16 @@
-//Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. 
+// Parte 1 -Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. 
 
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: "Sim",
   };
 
   console.log(`Bem vinda, ${info.personagem}`);
+  
+  // Parte 2 - Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. 
+  
+  console.log(info);
+
+  

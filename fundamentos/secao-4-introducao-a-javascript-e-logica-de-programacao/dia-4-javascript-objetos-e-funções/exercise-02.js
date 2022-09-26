0f77fd425d9b28361a50555f4eb379020f0bcf89
anadolacio <anadolacio@hotmail.com>
2022-09-25 23:33:35 -0300

    resolução do exercicio 02 parte 2 do dia 4

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funções/exercise-02.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funções/exercise-02.js
@@ -1,6 +1,6 @@
-// Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+// Parte 1 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
 
-// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
+// Parte 2 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
 
 let leitor = {
     nome: 'Julia',
@@ -12,7 +12,17 @@ let leitor = {
         autor: 'Daniela Kopsch',
         editora: 'Tordesilhas',
       },
-    ],
+      ],
   };
 
   console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama "${leitor['livrosFavoritos'][0]['titulo']}".`);
+
+  leitor.livrosFavoritos.push(
+    {
+      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+      autor: 'JK Rowling',
+      editor: 'Rocco',
+    },
+  );
+
+  console.log(`${leitor.nome} tem ${leitor['livrosFavoritos'].length} livros favoritos`);
